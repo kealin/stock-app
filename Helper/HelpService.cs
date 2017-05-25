fec1630e6462c4fdde73c79a537613135c095d20
Kim Lindqvist <k.lindqvist@gmail.com>
2017-05-25 14:00:11 +0300

    Initial commit on helper service

---
+++ Helper/HelpService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockPrices.Helper
+{
+    public class HelpService
+    {
+        /// <summary>
+        /// Builds predicate based on user input
+        /// </summary>
+        /// <remarks>
+        /// Currently supports ticker list only
+        /// </remarks>
+        public String BuildPredicate()
+        {
+            //http://finance.yahoo.com/d/quotes.csv?s=+ +&f=snbaopl
+            return "thepredicate";
+        }
+    }
+}
